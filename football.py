1fc0d8b0e82d299c0d5cb5d4e63112c13fd42024
Dan Rausch <dan.rausch@gmail.com>
2015-10-17 22:14:13 -0400

    Added initial Excel parsing
    Read the input Excel spreadsheet and find the week played.

--- football.py
+++ football.py
@@ -1,11 +1,34 @@
 import nflgame
+import argparse
+import xlrd
+import string
 
-games = nflgame.games(2015,week=1)
-games_gen = nflgame.games_gen(2015,week=1)
+parser= argparse.ArgumentParser(description='NFL Pick10 status.')
+
+parser.add_argument('-i', '--input', help='Weekly Excel Spreadsheet', required=True)
+
+args = parser.parse_args()
+
+wb = xlrd.open_workbook(args.input)
+
+print wb
+
+sheet_names = wb.sheet_names()
+print('Sheet Names', sheet_names)
+
+sheet = wb.sheet_by_name('Weekly Sheet')
+
+week_cell = sheet.cell(0,0)
+
+print week_cell
+
+week = string.split(week_cell.value)[1]
+
+games = nflgame.games(2015,week=int(week))
 
 for g in games:
-    print g.home
     print g.away
     print g.score_away
+    print g.home
     print g.score_home
 
